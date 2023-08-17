f294ab11fb72e4b598fe22824f89b63f8e9f39e4
David Sisson <EpsilonPrime@users.noreply.github.com>
2023-08-17 15:11:03 -0700

    Conditionally populates fuzztest if it's not already done.

--- third_party/fuzztest.cmake
+++ third_party/fuzztest.cmake
@@ -6,22 +6,23 @@ if (${CMAKE_BUILD_TYPE} STREQUAL "Debug")
             SOURCE_DIR "${TOPLEVEL_SOURCE_DIR}/third_party/fuzztest/"
     )
 
+    if(NOT fuzztest_POPULATD)
+        add_compile_options(-fsanitize=undefined)
+        add_link_options(-fsanitize=undefined)
 
-    add_compile_options(-fsanitize=undefined)
-    add_link_options(-fsanitize=undefined)
+        add_compile_options(-fsanitize-undefined-strip-path-components=-4)
+        set(ENV{UBSAN_OPTIONS} suppressions=${TOPLEVEL_SOURCE_DIR}/ubsan.supp)
+        set(ENV{ASAN_OPTIONS} suppressions=${TOPLEVEL_SOURCE_DIR}/asan.supp)
 
-    add_compile_options(-fsanitize-undefined-strip-path-components=-4)
-    set(ENV{UBSAN_OPTIONS} suppressions=${TOPLEVEL_SOURCE_DIR}/ubsan.supp)
-    set(ENV{ASAN_OPTIONS} suppressions=${TOPLEVEL_SOURCE_DIR}/asan.supp)
+        add_compile_options(-fsanitize=address)
+        add_link_options(-fsanitize=address)
 
-    add_compile_options(-fsanitize=address)
-    add_link_options(-fsanitize=address)
-
-    add_compile_options(-fsanitize-ignorelist=${TOPLEVEL_SOURCE_DIR}/sanitize-ignore-list.txt)
-    add_link_options(-fsanitize-ignorelist=${TOPLEVEL_SOURCE_DIR}/sanitize-ignore-list.txt)
+        add_compile_options(-fsanitize-ignorelist=${TOPLEVEL_SOURCE_DIR}/sanitize-ignore-list.txt)
+        add_link_options(-fsanitize-ignorelist=${TOPLEVEL_SOURCE_DIR}/sanitize-ignore-list.txt)
 
 
-    FetchContent_MakeAvailable(fuzztest)
+        FetchContent_MakeAvailable(fuzztest)
 
-    enable_testing()
+        enable_testing()
+    endif()
 endif ()
